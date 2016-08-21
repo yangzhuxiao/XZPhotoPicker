d13ef9a7e49ede0c95d778817145017d140c66b5
Jianing Zheng <jnzheng@CNjnzheng-2.local>
2016-08-21 20:53:47 +0800

    created BottomToolBarView

--- XZPhotoPicker/XZPhotoCollectionController.swift
+++ XZPhotoPicker/XZPhotoCollectionController.swift
@@ -13,6 +13,12 @@ import Photos
 
 class XZPhotoCollectionController: UIViewController {
     private var collectionView: UICollectionView?
+    private var previewButton = UIButton()
+    private var okButton = UIButton()
+    private let numberOfSelectedLabel = UILabel()
+    private let circleOfNumberImageView = UIImageView()
+    private let toolBarView = UIView()
+    
     var model: XZAlbumModel
     
     required init(model: XZAlbumModel) {
@@ -37,9 +43,10 @@ private extension XZPhotoCollectionController {
     func setup() {
         title = model.name
         setupCollectionView()
+        setupBottomToolBar()
     }
     
-    private func setupCollectionView() {
+    func setupCollectionView() {
         func collectionViewFlowLayout() -> UICollectionViewFlowLayout {
             let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
             flowLayout.itemSize = CGSize(width: CGFloat(PhotoCollectionCell_PhotoWidth),
@@ -62,16 +69,32 @@ private extension XZPhotoCollectionController {
             view.addSubview(collectionView!)
         }
     }
+    
+    func setupBottomToolBar() {
+        view.addSubview(toolBarView)
+        
+        toolBarView.addSubview(previewButton)
+        toolBarView.addSubview(circleOfNumberImageView)
+        toolBarView.addSubview(numberOfSelectedLabel)
+        toolBarView.addSubview(okButton)
+    }
 }
 
 // MARK: Layout
 private extension XZPhotoCollectionController {
     func layoutView() {
-        constrain(collectionView!) { (view) in
-            view.top == view.superview!.top
-            view.left == view.superview!.left
-            view.right == view.superview!.right
-            view.bottom == view.superview!.bottom
+        constrain(toolBarView) { (view) in
+            view.left == view.superview!.left - 1
+            view.right == view.superview!.right + 1
+            view.bottom == view.superview!.bottom + 1
+            view.height == PhotoCollection_BottomToolBarHeight
+        }
+        
+        constrain(collectionView!, toolBarView) { (view1, view2) in
+            view1.top == view1.superview!.top
+            view1.left == view1.superview!.left
+            view1.right == view1.superview!.right
+            view1.bottom == view2.top
         }
     }
 }
@@ -80,7 +103,13 @@ private extension XZPhotoCollectionController {
 private extension XZPhotoCollectionController {
     func style() {
         view.backgroundColor = UIColor.whiteColor()
+        view.clipsToBounds = true
+        
         collectionView!.backgroundColor = UIColor.whiteColor()
+        
+        toolBarView.backgroundColor = PhotoCollection_BottomToolBarBgColor
+        toolBarView.layer.borderColor = PhotoCollection_BottomToolBarSeparatorColor.CGColor
+        toolBarView.layer.borderWidth = 0.5
     }
 }
 
